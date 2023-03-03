3b7dfe0fbea31af735469ddee295da634b5ba930
Ajay Ranabhat <123541587+ajayeeposit@users.noreply.github.com>
2023-03-03 13:38:44 +0545

    Update notifyEmployee.js

--- controller/attendance/notifyEmployee.js
+++ controller/attendance/notifyEmployee.js
@@ -101,11 +101,11 @@ const notifyEmployeesMorning = async (req,res) => {
         }
       })
     );
-   
+    res.status(200).json({ message: "User Notified Successfully" })
   }
   catch (error) {
     console.log(error)
-    
+     res.status(500).json({ message: "Something went wrong" })
   }
 };
 
